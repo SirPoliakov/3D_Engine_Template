9342de372083879820df8117dc24671e422a1078
Ben <blambert@artfx.fr>
2024-05-28 14:29:27 +0200

    Necessary

---
+++ New/3DEngine2nd/src/Main.cpp
@@ -0,0 +1,28 @@
+#include <SDL.h>
+
+int main(int argc, char** argv)
+{
+	//Screen dimension constants
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 450;
+
+	// Window pointer
+	SDL_Window* window = nullptr;
+
+	// Initialize SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// Create window
+	window = SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	// Wait 2 sec
+	SDL_Delay(2000);
+
+	// Destroy window
+	SDL_DestroyWindow(window);
+
+	// Quit SDL
+	SDL_Quit();
+
+	return 0;
+}
